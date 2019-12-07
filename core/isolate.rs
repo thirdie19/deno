thirdie19/deno

commit 55102721f9bf0f805ce1af0e8bba70288980aa79

Use futures 0.3 API (#3358)

core/modules.rs
@@ -14,21 +14,22 @@ use crate::isolate::SourceCodeInfo;
 use crate::libdeno::deno_dyn_import_id;
 use crate::libdeno::deno_mod;
 use crate::module_specifier::ModuleSpecifier;
-use futures::future::loop_fn;
-use futures::future::Loop;
+use futures::future::FutureExt;
 use futures::stream::FuturesUnordered;
 use futures::stream::Stream;
-use futures::Async::*;
-use futures::Future;
-use futures::Poll;
+use futures::stream::TryStreamExt;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt;
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::task::Context;
+use std::task::Poll;
 
 pub type SourceCodeInfoFuture =
-  dyn Future<Item = SourceCodeInfo, Error = ErrBox> + Send;
+  dyn Future<Output = Result<SourceCodeInfo, ErrBox>> + Send;
 
 pub trait Loader: Send + Sync {
   /// Returns an absolute URL.
@@ -47,7 +48,7 @@ pub trait Loader: Send + Sync {
   fn load(
     &self,
     module_specifier: &ModuleSpecifier,
-  ) -> Box<SourceCodeInfoFuture>;
+  ) -> Pin<Box<SourceCodeInfoFuture>>;
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -68,16 +69,16 @@ enum State {
 /// This future is used to implement parallel async module loading without
 /// complicating the Isolate API.
 /// TODO: RecursiveLoad desperately needs to be merged with Modules.
-pub struct RecursiveLoad<L: Loader> {
+pub struct RecursiveLoad<L: Loader + Unpin> {
   kind: Kind,
   state: State,
   loader: L,
   modules: Arc<Mutex<Modules>>,
-  pending: FuturesUnordered<Box<SourceCodeInfoFuture>>,
+  pending: FuturesUnordered<Pin<Box<SourceCodeInfoFuture>>>,
   is_pending: HashSet<ModuleSpecifier>,
 }
 
-impl<L: Loader> RecursiveLoad<L> {
+impl<L: Loader + Unpin> RecursiveLoad<L> {
   /// Starts a new parallel load of the given URL of the main module.
   pub fn main(
     specifier: &str,
@@ -153,7 +154,7 @@ impl<L: Loader> RecursiveLoad<L> {
     // integrated into one thing.
     self
       .pending
-      .push(Box::new(self.loader.load(&module_specifier)));
+      .push(self.loader.load(&module_specifier).boxed());
     self.state = State::LoadingRoot;
 
     Ok(())
@@ -182,7 +183,7 @@ impl<L: Loader> RecursiveLoad<L> {
     {
       self
         .pending
-        .push(Box::new(self.loader.load(&module_specifier)));
+        .push(self.loader.load(&module_specifier).boxed());
       self.is_pending.insert(module_specifier);
     }
 
@@ -194,26 +195,24 @@ impl<L: Loader> RecursiveLoad<L> {
   pub fn get_future(
     self,
     isolate: Arc<Mutex<Isolate>>,
-  ) -> impl Future<Item = deno_mod, Error = ErrBox> {
-    loop_fn(self, move |load| {
-      let isolate = isolate.clone();
-      load.into_future().map_err(|(e, _)| e).and_then(
-        move |(event, mut load)| {
-          Ok(match event.unwrap() {
-            Event::Fetch(info) => {
-              let mut isolate = isolate.lock().unwrap();
-              load.register(info, &mut isolate)?;
-              Loop::Continue(load)
-            }
-            Event::Instantiate(id) => Loop::Break(id),
-          })
-        },
-      )
-    })
+  ) -> impl Future<Output = Result<deno_mod, ErrBox>> {
+    async move {
+      let mut load = self;
+      loop {
+        let event = load.try_next().await?;
+        match event.unwrap() {
+          Event::Fetch(info) => {
+            let mut isolate = isolate.lock().unwrap();
+            load.register(info, &mut isolate)?;
+          }
+          Event::Instantiate(id) => return Ok(id),
+        }
+      }
+    }
   }
 }
 
-impl<L: Loader> ImportStream for RecursiveLoad<L> {
+impl<L: Loader + Unpin> ImportStream for RecursiveLoad<L> {
   // TODO: this should not be part of RecursiveLoad.
   fn register(
     &mut self,
@@ -308,40 +307,45 @@ impl<L: Loader> ImportStream for RecursiveLoad<L> {
   }
 }
 
-impl<L: Loader> Stream for RecursiveLoad<L> {
-  type Item = Event;
-  type Error = ErrBox;
+impl<L: Loader + Unpin> Stream for RecursiveLoad<L> {
+  type Item = Result<Event, ErrBox>;
 
-  fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-    Ok(match self.state {
+  fn poll_next(
+    self: Pin<&mut Self>,
+    cx: &mut Context,
+  ) -> Poll<Option<Self::Item>> {
+    let inner = self.get_mut();
+    match inner.state {
       State::ResolveMain(ref specifier, Some(ref code)) => {
-        let module_specifier = self.loader.resolve(
+        let module_specifier = inner.loader.resolve(
           specifier,
           ".",
           true,
-          self.dyn_import_id().is_some(),
+          inner.dyn_import_id().is_some(),
         )?;
         let info = SourceCodeInfo {
           code: code.to_owned(),
           module_url_specified: module_specifier.to_string(),
           module_url_found: module_specifier.to_string(),
         };
-        self.state = State::LoadingRoot;
-        Ready(Some(Event::Fetch(info)))
+        inner.state = State::LoadingRoot;
+        Poll::Ready(Some(Ok(Event::Fetch(info))))
       }
       State::ResolveMain(..) | State::ResolveImport(..) => {
-        self.add_root()?;
-        self.poll()?
+        if let Err(e) = inner.add_root() {
+          return Poll::Ready(Some(Err(e)));
+        }
+        inner.try_poll_next_unpin(cx)
       }
       State::LoadingRoot | State::LoadingImports(..) => {
-        match self.pending.poll()? {
-          Ready(None) => unreachable!(),
-          Ready(Some(info)) => Ready(Some(Event::Fetch(info))),
-          NotReady => NotReady,
+        match inner.pending.try_poll_next_unpin(cx)? {
+          Poll::Ready(None) => unreachable!(),
+          Poll::Ready(Some(info)) => Poll::Ready(Some(Ok(Event::Fetch(info)))),
+          Poll::Pending => Poll::Pending,
         }
       }
-      State::Instantiated(id) => Ready(Some(Event::Instantiate(id))),
-    })
+      State::Instantiated(id) => Poll::Ready(Some(Ok(Event::Instantiate(id)))),
+    }
   }
 }
 
@@ -603,9 +607,11 @@ mod tests {
   use super::*;
   use crate::isolate::js_check;
   use crate::isolate::tests::*;
-  use futures::Async;
+  use futures::future::FutureExt;
+  use futures::stream::StreamExt;
   use std::error::Error;
   use std::fmt;
+  use std::future::Future;
 
   struct MockLoader {
     pub loads: Arc<Mutex<Vec<String>>>,
@@ -676,27 +682,27 @@ mod tests {
   }
 
   impl Future for DelayedSourceCodeFuture {
-    type Item = SourceCodeInfo;
-    type Error = ErrBox;
+    type Output = Result<SourceCodeInfo, ErrBox>;
 
-    fn poll(&mut self) -> Poll<Self::Item, ErrBox> {
-      self.counter += 1;
-      if self.url == "file:///never_ready.js" {
-        return Ok(Async::NotReady);
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+      let inner = self.get_mut();
+      inner.counter += 1;
+      if inner.url == "file:///never_ready.js" {
+        return Poll::Pending;
       }
-      if self.url == "file:///slow.js" && self.counter < 2 {
+      if inner.url == "file:///slow.js" && inner.counter < 2 {
         // TODO(ry) Hopefully in the future we can remove current task
         // notification. See comment above run_in_task.
-        futures::task::current().notify();
-        return Ok(Async::NotReady);
+        cx.waker().wake_by_ref();
+        return Poll::Pending;
       }
-      match mock_source_code(&self.url) {
-        Some(src) => Ok(Async::Ready(SourceCodeInfo {
+      match mock_source_code(&inner.url) {
+        Some(src) => Poll::Ready(Ok(SourceCodeInfo {
           code: src.0.to_owned(),
-          module_url_specified: self.url.clone(),
+          module_url_specified: inner.url.clone(),
           module_url_found: src.1.to_owned(),
         })),
-        None => Err(MockError::LoadErr.into()),
+        None => Poll::Ready(Err(MockError::LoadErr.into())),
       }
     }
   }
@@ -733,11 +739,11 @@ mod tests {
     fn load(
       &self,
       module_specifier: &ModuleSpecifier,
-    ) -> Box<SourceCodeInfoFuture> {
+    ) -> Pin<Box<SourceCodeInfoFuture>> {
       let mut loads = self.loads.lock().unwrap();
       loads.push(module_specifier.to_string());
       let url = module_specifier.to_string();
-      Box::new(DelayedSourceCodeFuture { url, counter: 0 })
+      DelayedSourceCodeFuture { url, counter: 0 }.boxed()
     }
   }
 
@@ -780,7 +786,7 @@ mod tests {
 
   #[test]
   fn test_recursive_load() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let modules = loader.modules.clone();
       let modules_ = modules.clone();
@@ -791,12 +797,12 @@ mod tests {
         RecursiveLoad::main("/a.js", None, loader, modules);
 
       let a_id = loop {
-        match recursive_load.poll() {
-          Ok(Ready(Some(Event::Fetch(info)))) => {
+        match recursive_load.try_poll_next_unpin(&mut cx) {
+          Poll::Ready(Some(Ok(Event::Fetch(info)))) => {
             let mut isolate = isolate.lock().unwrap();
             recursive_load.register(info, &mut isolate).unwrap();
           }
-          Ok(Ready(Some(Event::Instantiate(id)))) => break id,
+          Poll::Ready(Some(Ok(Event::Instantiate(id)))) => break id,
           _ => panic!("unexpected result"),
         };
       };
@@ -859,7 +865,7 @@ mod tests {
 
   #[test]
   fn test_circular_load() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let isolate = loader.isolate.clone();
       let isolate_ = isolate.clone();
@@ -868,9 +874,10 @@ mod tests {
       let loads = loader.loads.clone();
       let recursive_load =
         RecursiveLoad::main("/circular1.js", None, loader, modules);
-      let result = recursive_load.get_future(isolate.clone()).poll();
-      assert!(result.is_ok());
-      if let Async::Ready(circular1_id) = result.ok().unwrap() {
+      let mut load_fut = recursive_load.get_future(isolate.clone()).boxed();
+      let result = Pin::new(&mut load_fut).poll(&mut cx);
+      assert!(result.is_ready());
+      if let Poll::Ready(Ok(circular1_id)) = result {
         let mut isolate = isolate_.lock().unwrap();
         js_check(isolate.mod_evaluate(circular1_id));
 
@@ -930,7 +937,7 @@ mod tests {
 
   #[test]
   fn test_redirect_load() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let isolate = loader.isolate.clone();
       let isolate_ = isolate.clone();
@@ -939,10 +946,11 @@ mod tests {
       let loads = loader.loads.clone();
       let recursive_load =
         RecursiveLoad::main("/redirect1.js", None, loader, modules);
-      let result = recursive_load.get_future(isolate.clone()).poll();
+      let mut load_fut = recursive_load.get_future(isolate.clone()).boxed();
+      let result = Pin::new(&mut load_fut).poll(&mut cx);
       println!(">> result {:?}", result);
-      assert!(result.is_ok());
-      if let Async::Ready(redirect1_id) = result.ok().unwrap() {
+      assert!(result.is_ready());
+      if let Poll::Ready(Ok(redirect1_id)) = result {
         let mut isolate = isolate_.lock().unwrap();
         js_check(isolate.mod_evaluate(redirect1_id));
         let l = loads.lock().unwrap();
@@ -995,18 +1003,18 @@ mod tests {
 
   #[test]
   fn slow_never_ready_modules() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let isolate = loader.isolate.clone();
       let modules = loader.modules.clone();
       let loads = loader.loads.clone();
       let mut recursive_load =
         RecursiveLoad::main("/main.js", None, loader, modules)
-          .get_future(isolate);
+          .get_future(isolate)
+          .boxed();
 
-      let result = recursive_load.poll();
-      assert!(result.is_ok());
-      assert!(result.ok().unwrap().is_not_ready());
+      let result = recursive_load.poll_unpin(&mut cx);
+      assert!(result.is_pending());
 
       // TODO(ry) Arguably the first time we poll only the following modules
       // should be loaded:
@@ -1018,9 +1026,8 @@ mod tests {
       // run_in_task.
 
       for _ in 0..10 {
-        let result = recursive_load.poll();
-        assert!(result.is_ok());
-        assert!(result.ok().unwrap().is_not_ready());
+        let result = recursive_load.poll_unpin(&mut cx);
+        assert!(result.is_pending());
         let l = loads.lock().unwrap();
         assert_eq!(
           l.to_vec(),
@@ -1045,19 +1052,22 @@ mod tests {
 
   #[test]
   fn loader_disappears_after_error() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let isolate = loader.isolate.clone();
       let modules = loader.modules.clone();
       let recursive_load =
         RecursiveLoad::main("/bad_import.js", None, loader, modules);
-      let result = recursive_load.get_future(isolate).poll();
-      assert!(result.is_err());
-      let err = result.err().unwrap();
-      assert_eq!(
-        err.downcast_ref::<MockError>().unwrap(),
-        &MockError::ResolveErr
-      );
+      let mut load_fut = recursive_load.get_future(isolate).boxed();
+      let result = load_fut.poll_unpin(&mut cx);
+      if let Poll::Ready(Err(err)) = result {
+        assert_eq!(
+          err.downcast_ref::<MockError>().unwrap(),
+          &MockError::ResolveErr
+        );
+      } else {
+        unreachable!();
+      }
     })
   }
 
@@ -1072,7 +1082,7 @@ mod tests {
 
   #[test]
   fn recursive_load_main_with_code() {
-    run_in_task(|| {
+    run_in_task(|mut cx| {
       let loader = MockLoader::new();
       let modules = loader.modules.clone();
       let modules_ = modules.clone();
@@ -1090,12 +1100,12 @@ mod tests {
       );
 
       let main_id = loop {
-        match recursive_load.poll() {
-          Ok(Ready(Some(Event::Fetch(info)))) => {
+        match recursive_load.poll_next_unpin(&mut cx) {
+          Poll::Ready(Some(Ok(Event::Fetch(info)))) => {
             let mut isolate = isolate.lock().unwrap();
             recursive_load.register(info, &mut isolate).unwrap();
           }
-          Ok(Ready(Some(Event::Instantiate(id)))) => break id,
+          Poll::Ready(Some(Ok(Event::Instantiate(id)))) => break id,
           _ => panic!("unexpected result"),
         };
       };

.rustfmt.toml
@@ -1,3 +1,4 @@
 # Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 max_width = 80
 tab_spaces = 2
+edition = "2018"

cli/Cargo.toml
@@ -31,7 +31,7 @@ base64 = "0.11.0"
 byteorder = "1.3.2"
 clap = "2.33.0"
 dirs = "2.0.2"
-futures = "0.1.29"
+futures = { version = "0.3", features = [ "compat", "io-compat" ] }
 http = "0.1.19"
 hyper = "0.12.35"
 hyper-rustls = "0.17.1"

cli/compilers/js.rs
@@ -2,6 +2,8 @@
 use crate::compilers::CompiledModule;
 use crate::compilers::CompiledModuleFuture;
 use crate::file_fetcher::SourceFile;
+use crate::futures::future::FutureExt;
+use std::pin::Pin;
 use std::str;
 
 pub struct JsCompiler {}
@@ -10,7 +12,7 @@ impl JsCompiler {
   pub fn compile_async(
     self: &Self,
     source_file: &SourceFile,
-  ) -> Box<CompiledModuleFuture> {
+  ) -> Pin<Box<CompiledModuleFuture>> {
     let module = CompiledModule {
       code: str::from_utf8(&source_file.source_code)
         .unwrap()
@@ -18,6 +20,6 @@ impl JsCompiler {
       name: source_file.url.to_string(),
     };
 
-    Box::new(futures::future::ok(module))
+    futures::future::ok(module).boxed()
   }
 }

cli/compilers/json.rs
@@ -2,8 +2,10 @@
 use crate::compilers::CompiledModule;
 use crate::compilers::CompiledModuleFuture;
 use crate::file_fetcher::SourceFile;
+use crate::futures::future::FutureExt;
 use deno::ErrBox;
 use regex::Regex;
+use std::pin::Pin;
 use std::str;
 
 // From https://github.com/mathiasbynens/mothereff.in/blob/master/js-variables/eff.js
@@ -15,11 +17,11 @@ impl JsonCompiler {
   pub fn compile_async(
     self: &Self,
     source_file: &SourceFile,
-  ) -> Box<CompiledModuleFuture> {
+  ) -> Pin<Box<CompiledModuleFuture>> {
     let maybe_json_value: serde_json::Result<serde_json::Value> =
       serde_json::from_str(&str::from_utf8(&source_file.source_code).unwrap());
     if let Err(err) = maybe_json_value {
-      return Box::new(futures::future::err(ErrBox::from(err)));
+      return futures::future::err(ErrBox::from(err)).boxed();
     }
 
     let mut code = format!(
@@ -50,6 +52,6 @@ impl JsonCompiler {
       name: source_file.url.to_string(),
     };
 
-    Box::new(futures::future::ok(module))
+    futures::future::ok(module).boxed()
   }
 }

cli/compilers/mod.rs
@@ -19,4 +19,4 @@ pub struct CompiledModule {
 }
 
 pub type CompiledModuleFuture =
-  dyn Future<Item = CompiledModule, Error = ErrBox> + Send;
+  dyn Future<Output = Result<CompiledModule, ErrBox>> + Send;

commit 0dab62e06e3aa3947ff93cf288377b8a4aef8bed

fix isolate tests (#3459)
Some tests were silently failing after #3358 and #3434 because pool.spawn_ok
was used which doesn't panic on errors. For reference, the failure looked like this:

thread '<unnamed>' panicked at 'assertion failed: match isolate.poll_unpin(cx) { Poll::Ready(Ok(_)) => true, _ => false, }', core/isolate.rs:1408:7

core/isolate.rs
@@ -759,7 +759,6 @@ pub fn js_check<T>(r: Result<T, ErrBox>) -> T {
 #[cfg(test)]
 pub mod tests {
   use super::*;
-  use futures::executor::ThreadPool;
   use futures::future::lazy;
   use std::io;
   use std::ops::FnOnce;
@@ -769,8 +768,7 @@ pub mod tests {
   where
     F: FnOnce(&mut Context) + Send + 'static,
   {
-    let poll = ThreadPool::new().unwrap();
-    poll.spawn_ok(lazy(move |cx| f(cx)));
+    futures::executor::block_on(lazy(move |cx| f(cx)));
   }
 
   fn poll_until_ready<F>(future: &mut F, max_poll_count: usize) -> F::Output
@@ -790,34 +788,8 @@ pub mod tests {
     )
   }
 
-  struct DelayedFuture {
-    counter: u32,
-    buf: Box<[u8]>,
-  }
-
-  impl DelayedFuture {
-    pub fn new(buf: Box<[u8]>) -> Self {
-      DelayedFuture { counter: 0, buf }
-    }
-  }
-
-  impl Future for DelayedFuture {
-    type Output = Result<Box<[u8]>, ()>;
-
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
-      let inner = self.get_mut();
-      if inner.counter > 0 {
-        return Poll::Ready(Ok(inner.buf.clone()));
-      }
-
-      inner.counter += 1;
-      Poll::Pending
-    }
-  }
-
   pub enum Mode {
-    AsyncImmediate,
-    AsyncDelayed,
+    Async,
     OverflowReqSync,
     OverflowResSync,
     OverflowReqAsync,
@@ -834,17 +806,11 @@ pub mod tests {
       move |control: &[u8], _zero_copy: Option<PinnedBuf>| -> CoreOp {
         dispatch_count_.fetch_add(1, Ordering::Relaxed);
         match mode {
-          Mode::AsyncImmediate => {
+          Mode::Async => {
             assert_eq!(control.len(), 1);
             assert_eq!(control[0], 42);
             let buf = vec![43u8, 0, 0, 0].into_boxed_slice();
             Op::Async(futures::future::ok(buf).boxed())
-          }
-          Mode::AsyncDelayed => {
-            assert_eq!(control.len(), 1);
-            assert_eq!(control[0], 42);
-            let buf = vec![43u8, 0, 0, 0].into_boxed_slice();
-            Op::Async(DelayedFuture::new(buf).boxed())
           }
           Mode::OverflowReqSync => {
             assert_eq!(control.len(), 100 * 1024 * 1024);
@@ -863,7 +829,7 @@ pub mod tests {
           Mode::OverflowReqAsync => {
             assert_eq!(control.len(), 100 * 1024 * 1024);
             let buf = vec![43u8, 0, 0, 0].into_boxed_slice();
-            Op::Async(DelayedFuture::new(buf).boxed())
+            Op::Async(futures::future::ok(buf).boxed())
           }
           Mode::OverflowResAsync => {
             assert_eq!(control.len(), 1);
@@ -872,7 +838,7 @@ pub mod tests {
             vec.resize(100 * 1024 * 1024, 0);
             vec[0] = 4;
             let buf = vec.into_boxed_slice();
-            Op::Async(DelayedFuture::new(buf).boxed())
+            Op::Async(futures::future::ok(buf).boxed())
           }
         }
       };
@@ -895,7 +861,7 @@ pub mod tests {
 
   #[test]
   fn test_dispatch() {
-    let (mut isolate, dispatch_count) = setup(Mode::AsyncImmediate);
+    let (mut isolate, dispatch_count) = setup(Mode::Async);
     js_check(isolate.execute(
       "filename.js",
       r#"
@@ -912,7 +878,7 @@ pub mod tests {
 
   #[test]
   fn test_mods() {
-    let (mut isolate, dispatch_count) = setup(Mode::AsyncImmediate);
+    let (mut isolate, dispatch_count) = setup(Mode::Async);
     let mod_a = isolate
       .mod_new(
         true,
@@ -959,59 +925,9 @@ pub mod tests {
   }
 
   #[test]
-  fn test_poll_async_immediate_ops() {
-    run_in_task(|cx| {
-      let (mut isolate, dispatch_count) = setup(Mode::AsyncImmediate);
-
-      js_check(isolate.execute(
-        "setup2.js",
-        r#"
-         let nrecv = 0;
-         Deno.core.setAsyncHandler((opId, buf) => {
-           nrecv++;
-         });
-         "#,
-      ));
-      assert_eq!(dispatch_count.load(Ordering::Relaxed), 0);
-      js_check(isolate.execute(
-        "check1.js",
-        r#"
-         assert(nrecv == 0);
-         let control = new Uint8Array([42]);
-         const res1 = Deno.core.send(1, control);
-         assert(res1);
-         assert(nrecv == 0);
-         "#,
-      ));
-      assert_eq!(dispatch_count.load(Ordering::Relaxed), 1);
-      assert_eq!(dispatch_count.load(Ordering::Relaxed), 1);
-      js_check(isolate.execute(
-        "check2.js",
-        r#"
-         assert(nrecv == 0);
-         Deno.core.send(1, control);
-         assert(nrecv == 0);
-         "#,
-      ));
-      assert_eq!(dispatch_count.load(Ordering::Relaxed), 2);
-      assert_eq!(dispatch_count.load(Ordering::Relaxed), 2);
-      assert!(match isolate.poll_unpin(cx) {
-        Poll::Ready(Ok(_)) => true,
-        _ => false,
-      });
-      js_check(isolate.execute("check3.js", "assert(nrecv == 0)"));
-      // We are idle, so the next poll should be the last.
-      assert!(match isolate.poll_unpin(cx) {
-        Poll::Ready(Ok(_)) => true,
-        _ => false,
-      });
-    });
-  }
-
-  #[test]
   fn test_poll_async_delayed_ops() {
     run_in_task(|cx| {
-      let (mut isolate, dispatch_count) = setup(Mode::AsyncDelayed);
+      let (mut isolate, dispatch_count) = setup(Mode::Async);
 
       js_check(isolate.execute(
         "setup2.js",
@@ -1275,7 +1191,7 @@ pub mod tests {
     let (tx, rx) = std::sync::mpsc::channel::<bool>();
     let tx_clone = tx.clone();
 
-    let (mut isolate, _dispatch_count) = setup(Mode::AsyncImmediate);
+    let (mut isolate, _dispatch_count) = setup(Mode::Async);
     let shared = isolate.shared_isolate_handle();
 
     let t1 = std::thread::spawn(move || {
@@ -1332,7 +1248,7 @@ pub mod tests {
   fn dangling_shared_isolate() {
     let shared = {
       // isolate is dropped at the end of this block
-      let (mut isolate, _dispatch_count) = setup(Mode::AsyncImmediate);
+      let (mut isolate, _dispatch_count) = setup(Mode::Async);
       isolate.shared_isolate_handle()
     };
 
@@ -1499,7 +1415,7 @@ pub mod tests {
   #[test]
   fn test_js() {
     run_in_task(|mut cx| {
-      let (mut isolate, _dispatch_count) = setup(Mode::AsyncImmediate);
+      let (mut isolate, _dispatch_count) = setup(Mode::Async);
       js_check(
         isolate.execute(
           "shared_queue_test.js",

core/modules.rs
@@ -626,7 +626,7 @@ mod tests {
   impl MockLoader {
     fn new() -> Self {
       let modules = Modules::new();
-      let (isolate, _dispatch_count) = setup(Mode::AsyncImmediate);
+      let (isolate, _dispatch_count) = setup(Mode::Async);
       Self {
         loads: Arc::new(Mutex::new(Vec::new())),
         isolate: Arc::new(Mutex::new(isolate)),